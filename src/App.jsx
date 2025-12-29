ee22b1942949f6ae5695ad0793f7c92e2f782dc2
theShihamAhamed <theShihamAhamed@gmail.com>
2025-12-29 23:52:11 +0530

    chore: initialize project and set up development environment

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-3xl text-indigo-800">
+      GSAP powered GTA VI landing page
+    </div>
+  );
+};
+
+export default App;
